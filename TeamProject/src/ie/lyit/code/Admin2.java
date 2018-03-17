ba719b92111dad51525e222e5a39de5c57e9e211
Ciarán Toman <L00120376@student.lyit.ie>
2018-03-17 06:56:16 +0000

    New GUI page - Admin2
    Created a new JFrame page to hold all panels.
    Created title label and added to frame.
    
    initialized panels.
    
    Added a draw method.

---
+++ TeamProject/src/ie/lyit/code/Admin2.java
@@ -0,0 +1,69 @@
+/* Author: Ciaran Toman
+ * Class: Cloud Computing
+ * DESC: GUI application Administrator second page.
+ * 				Methods:
+ * 						- Draw GUI
+ * 
+ * 
+ */	
+
+
+package ie.lyit.code;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Admin2 extends JFrame {
+	
+	//instance fields
+	//panels
+	private JPanel centerPanel;
+	private AdminPanelBuilder apb1, apb2;
+	//panel type
+	private String add = "add";
+	private String remove = "remove";
+	
+	//labels
+	private JLabel titleLabel = new JLabel("Simple Shopping System");
+	
+	
+	private Font titleFont = new Font("SanSerif", Font.ITALIC, 40);
+	
+	//constructor
+	public Admin2() {
+		
+		//create title label and add to frame
+		titleLabel.setFont(titleFont);
+		add(titleLabel, BorderLayout.NORTH);
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+		
+		centerPanel = new JPanel(new GridLayout(2, 1, 0, 10));
+		//create panels
+		apb1 = new AdminPanelBuilder(add);
+		apb2 = new AdminPanelBuilder(remove);
+	}
+	
+	//draw GUI
+	public static void drawAdmin2() {
+		
+		Admin2 a2 = new Admin2();
+		a2.setTitle("Administrator");		
+		//op.pack();
+		a2.setSize(500, 300);
+		a2.setLocationRelativeTo(null);
+		a2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		a2.setVisible(true);
+		a2.requestFocusInWindow(true);
+	}
+	
+	//main
+	public static void main(String[] args) {
+		drawAdmin2();
+	}
+
+}
