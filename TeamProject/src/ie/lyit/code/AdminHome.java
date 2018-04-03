93a19254b389a0564d7cb156e56a265b0975b764
Ciarán Toman <L00120376@student.lyit.ie>
2018-04-03 22:35:49 +0100

    AdminHome page update
    Added new south panel.
    
    Added two new buttons to that panel
    - exit and go back.
    
    Disabled resizing.
    
    Added inner listener class and applied
    it to the two buttons.
    - functionality completed.

--- TeamProject/src/ie/lyit/code/AdminHome.java
+++ TeamProject/src/ie/lyit/code/AdminHome.java
@@ -1,9 +1,21 @@
+/* Author: Ciaran Toman
+ * Class: Cloud Computing
+ * DESC: GUI application Admin Home page.
+ * 
+ * 
+ * 
+ */
+
 package ie.lyit.code;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,18 +23,22 @@ import javax.swing.JPanel;
 public class AdminHome extends JFrame{
 
 	//instance fields
-	private JPanel northPanel, centerPanel;
+	private JPanel northPanel, centerPanel, southPanel;
 	
 	//labels
 	private JLabel titleLabel = new JLabel("Simple Shopping System");
 	
 	//font
 	private Font titleFont = new Font("SanSerif", Font.ITALIC, 40);
+	private Font generalFont = new Font("SanSerif", Font.BOLD, 22);
 	
+	//button
+	private JButton backBtn, exitBtn;
 	
 	//constructor
 	public AdminHome() {
 		
+		//title panel
 		northPanel = new JPanel();
 		
 		titleLabel.setFont(titleFont);
@@ -31,7 +47,66 @@ public class AdminHome extends JFrame{
 		
 		add(northPanel, BorderLayout.NORTH);
 		
+		
+		
+		//south panel
+		southPanel = new JPanel(new FlowLayout(1, 240, 10));
+		
+		backBtn = new JButton("Back/Logout");
+		backBtn.setFont(generalFont);
+		
+		exitBtn = new JButton("Exit Application");
+		exitBtn.setFont(generalFont);
+		
+		southPanel.add(backBtn);
+		southPanel.add(exitBtn);
+		
+		add(southPanel, BorderLayout.SOUTH);
+		
+		
+		
+		
+		
+		
+		
+		//add listeners
+		backBtn.addActionListener(new ActionListenerClass());
+		exitBtn.addActionListener(new ActionListenerClass());
+		
+		
+		//disable resizing
+		setResizable(false);
 	}
+	
+	
+	
+	
+	//inner action listener
+	public class ActionListenerClass implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			Object event = e.getSource();
+
+			//if event equals back button
+			if(event == backBtn) {
+				
+				//dispose this page
+				dispose();
+				
+				//open login page
+				Admin1.drawAdmin1();
+			}
+			//if exit button
+			else if(event == exitBtn) {
+				
+				//dispose this page
+				dispose();
+			}
+		}
+	}
+	
 	
 	
 	
